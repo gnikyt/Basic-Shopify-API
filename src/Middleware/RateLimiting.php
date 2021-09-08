162d6fb662b3c583d22e2e66ef2faed6b62d23de
Robert Tolton <rob@roberttolton.com>
2021-09-08 09:24:41 +0100

    Remove use of 'time' index from rate limit cache data
    Update documentation to clearly state that the time and limit storages should be separate instances, otherwise they will poison each other if the same instance is used like what was originally suggested
    Update tests

--- src/Middleware/RateLimiting.php
+++ src/Middleware/RateLimiting.php
@@ -56,13 +56,7 @@ class RateLimiting extends AbstractMiddleware
         }
 
         // Determine if this call has passed the window
-        $firstTime = end($times);
-
-        if (is_array($firstTime)) {
-            $firstTime = $firstTime['time'];
-        }
-
-        $windowTime = $firstTime + 1000000;
+        $windowTime = end($times) + 1000000;
         $currentTime = $td->getCurrentTime();
 
         if ($currentTime > $windowTime) {
